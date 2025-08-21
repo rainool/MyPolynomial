51ad46c4544bb8ba76262d604921f8e85f3518f5
rainool <203097192+rainool@users.noreply.github.com>
2025-08-21 11:49:03 +1000

    Update TestMyPolynomial.cs

--- TestMyPolynomial.cs
+++ TestMyPolynomial.cs
@@ -27,33 +27,32 @@
 
                 //To operate on the polynomials, take the object name (poly1, poly2), and add the method onto the end (poly1.Add()) and put it into the try-catch block, as there is built in error handling.
 
+                Console.WriteLine("Polynomial 1 Nth-Degree: " + poly1.GetDegree() + "\n");
+                //Expected result: 2
+
+                Console.WriteLine("Polynomial 1: " + poly1.ToString() + "\n");
+                //Expected result: -3x^2 + 2x + 1
+
+                Console.WriteLine("Polynomial 2: " + poly2.ToString() + "\n");
+                //Expected result: -4x^3 + 3x^2 + 2x + 1
+
+                Console.WriteLine("Evaluate Polynomial1(2): " + poly1.Evaluate(2) + "\n");
+                //Expected result: f(2) = -7
+
+                Console.WriteLine("Add: (-3x^2 + 2x + 1) + (-4x^3 + 3x^2 + 2x + 1): " + poly1.Add(poly2) + "\n");
+                //Expected result: -4x ^ 3 + 4x + 2
+
+                Console.WriteLine("Multiply: (-3x^2 + 2x + 1) * (-4x^3 + 3x^2 + 2x + 1): " + poly1.Multiply(poly2) + "\n");
+                //Expected result: 12x^5 - 17x^4 - 4x^3 + 4x^2 + 4x + 1
             }
             catch (ArgumentException exception)
             {
                 Console.WriteLine("The following error detected: " 
                     + exception.GetType().ToString() + " with message \"" + exception.Message + "\"");
             }
-
-            Console.WriteLine("Polynomial 1 Nth-Degree: " + poly1.GetDegree() + "\n");
-            //Expected result: 2
-
-            Console.WriteLine("Polynomial 1: " + poly1.ToString() + "\n");
-            //Expected result: -3x^2 + 2x + 1
-
-            Console.WriteLine("Polynomial 2: " + poly2.ToString() + "\n");
-            //Expected result: -4x^3 + 3x^2 + 2x + 1
-
-            Console.WriteLine("Evaluate Polynomial1(2): " + poly1.Evaluate(2) + "\n");
-            //Expected result: f(2) = -7
-
-            Console.WriteLine("Add: (-3x^2 + 2x + 1) + (-4x^3 + 3x^2 + 2x + 1): " + poly1.Add(poly2) + "\n");
-            //Expected result: -4x ^ 3 + 4x + 2
-
-            Console.WriteLine("Multiply: (-3x^2 + 2x + 1) * (-4x^3 + 3x^2 + 2x + 1): " + poly1.Multiply(poly2) + "\n");
-            //Expected result: 12x^5 - 17x^4 - 4x^3 + 4x^2 + 4x + 1
-
         }
     }
 }
+
 
 
