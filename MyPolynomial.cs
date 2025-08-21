d05d5de5647c88b2f00cc5d8390595c7ff90ab3e
rainool <203097192+rainool@users.noreply.github.com>
2025-08-21 11:44:49 +1000

    Add files via upload

---
+++ MyPolynomial.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polynomial
+{
+    internal class MyPolynomial
+    {
+        private double[] _coeffs;
+
+        public MyPolynomial(double[] coeffs)
+        {
+            //Null or empty array validation
+            if (coeffs == null || coeffs.Length == 0)
+            {
+                throw new ArgumentException("Coefficients cannot be null or empty");
+            }
+            _coeffs = new double[coeffs.Length];
+
+            //array population
+            for (int i = 0; i < coeffs.Length; i++)
+            {
+                _coeffs[i] = coeffs[i];
+            }
+        }
+
+        public int GetDegree()
+        {
+            //Finds the degree by -1 from the amount of elements.
+            //(ie. quadratic expression has 2 indeterminate and 1 constant, ax^2 + bx + c, hence the degree is 2)
+            for (int i = _coeffs.Length - 1; i >= 0; i--)
+            {
+                if (_coeffs[i] != 0)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder(); 
+            int degree = GetDegree();
+            bool firstTerm = true;
+
+            for (int i = degree; i >= 0; i--) //starts from the variable with highest degree
+            {
+                double coef = _coeffs[i]; //coef = highest degree variables coefficient
+                if (coef == 0) //if the coefficient is 0 ( 0x^n), then it skips to next lowest degree
+                {
+                    continue;
+                }
+
+                if (firstTerm)
+                {
+                    if (coef < 0) // checking for negative cooefficients
+                    {
+                        sb.Append("-");
+                    }
+                }
+                else
+                {
+                    sb.Append(coef < 0 ? " - " : " + "); //appending negative or positive signs for each coefficient
+                }
+                
+                //absolute coefficient. Multiplies negative coefficients with -1 to get
+                //an absolute coefficient
+                double absCoef = coef < 0 ? -coef : coef; 
+
+
+                //Checks for the degree, x^0 = 1
+                //If absolute = 1, then no variables with exponentials need to be attached
+                if (absCoef != 1 || i == 0)
+                {
+                    sb.Append(absCoef);
+                }
+
+                //if absolute coefficient does have a degree, it attaches x, and then checks for the degree (i)
+                //and attaches to coefficient with plaintext format (x^n)
+                if (i > 0) 
+                {
+                    sb.Append("x");
+                    
+                    if (i > 1)
+                    {
+                        sb.Append("^" + i);
+                    }
+                }
+
+                firstTerm = false;
+
+            }
+            //If polynomial has any length >= 1 (constant only or nth degree)
+            //it will return the built string, else it will return a 0 (no constant or exponential variables
+            return sb.Length > 0 ? sb.ToString() : "0"; 
+        }
+
+        public double Evaluate(double x)
+        {
+            //local exponentiation method to fit UML constraints
+            double Power(double baseValue, int exponent)
+            {
+                double result = 1; //if degree = 0, x^0 = 1
+                for(int i = 0; i < exponent; i++)
+                {
+                    result *= baseValue;
+                }
+                return result;
+            }
+
+            //substituting each x with the ith iteration (x = b, where b is some arbitrary value)
+            double result = 0;
+            for (int i = 0; i < _coeffs.Length; i++)
+            {
+                result += _coeffs[i] * Power(x, i);
+            }
+            return result;
+        }
+
+        public MyPolynomial Add(MyPolynomial another)
+        {
+            //checking which polynomial is longer. The longer polynomial will be the max length
+            //after the sum of both polynomials
+            int maxLen = _coeffs.Length > another._coeffs.Length ? _coeffs.Length : another._coeffs.Length;
+            double[] result = new double[maxLen];
+
+      
+            for (int i = 0; i < maxLen; i++)
+            {
+                //if the iteration is higher than the maxLen (which dictates the highest degree in the addition system)
+                //then it adds zero
+                double a = i < _coeffs.Length ? _coeffs[i] : 0; 
+                double b = i < another._coeffs.Length ? another._coeffs[i] : 0;
+                result[i] = a + b;
+            }
+            return new MyPolynomial(result);
+        }
+
+        public MyPolynomial Multiply(MyPolynomial another)
+        {
+            int resultLen = _coeffs.Length + another._coeffs.Length - 1;
+            double[] result = new double[resultLen];
+
+            //takes each element in both polynomials, and multiplies by each other to get the product
+            //of both polynomials
+            for (int i = 0; i < _coeffs.Length; i++)
+            {
+                for (int j = 0; j < another._coeffs.Length; j++)
+                {
+                    result[i + j] += _coeffs[i] * another._coeffs[j];
+                }
+            }
+            return new MyPolynomial(result);
+        }
+
+    }
+}
---
+++ TestMyPolynomial.cs
@@ -0,0 +1,59 @@
+﻿namespace polynomial
+{
+    internal class TestMyPolynomial
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = new double[3];
+            arr[0] = 1; //constant
+            arr[1] = 2; // x
+            arr[2] = -3; // i = n-th degree
+
+            double[] arr1 = new double[4];
+            arr1[0] = 1; //constant
+            arr1[1] = 2; // x
+            arr1[2] = 3; // i = n-th degree
+            arr1[3] = -4;
+            
+
+            try
+            {
+                MyPolynomial poly1 = new MyPolynomial(arr); // -3x^2 + 2x + 1
+                MyPolynomial poly2 = new MyPolynomial(arr1); // -4x^3 + 3x^2 + 2x + 1
+
+                Console.WriteLine("Multiply: (-3x^2 + 2x + 1) * (-4x^3 + 3x^2 + 2x + 1): " + poly1.Multiply(poly2) + "\n");
+                //Expected result: 12x^5 - 17x^4 - 4x^3 + 4x^2 + 4x + 1
+
+
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine("The following error detected: " 
+                    + exception.GetType().ToString() + " with message \"" + exception.Message + "\"");
+            }
+
+
+
+
+
+            //Console.WriteLine("Polynomial 1 Nth-Degree: " + poly1.GetDegree() + "\n");
+            ////Expected result: 2
+
+            //Console.WriteLine("Polynomial 1: " + poly1.ToString() + "\n");
+            ////Expected result: -3x^2 + 2x + 1
+
+            //Console.WriteLine("Polynomial 2: " + poly2.ToString() + "\n");
+            ////Expected result: -4x^3 + 3x^2 + 2x + 1
+
+            //Console.WriteLine("Evaluate Polynomial1(2): " + poly1.Evaluate(2) + "\n");
+            ////Expected result: f(2) = -7
+
+            //Console.WriteLine("Add: (-3x^2 + 2x + 1) + (-4x^3 + 3x^2 + 2x + 1): " + poly1.Add(poly2) + "\n");
+            ////Expected result: -4x ^ 3 + 4x + 2
+
+            //Console.WriteLine("Multiply: (-3x^2 + 2x + 1) * (-4x^3 + 3x^2 + 2x + 1): " + poly1.Multiply(poly2) + "\n");
+            ////Expected result: 12x^5 - 17x^4 - 4x^3 + 4x^2 + 4x + 1
+
+        }
+    }
+}
